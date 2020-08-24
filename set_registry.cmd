fd944d4a29bf0a64fad582f5e77bc348ec24e2d6
Maksim Beliaev <maksim.beliaev@ansys.com>
2020-08-24 13:13:06 +0200

    added full automated solution to add PS cpntext menu

---
+++ set_registry.cmd
@@ -0,0 +1,19 @@
+SetACL.exe -on "HKEY_CLASSES_ROOT\Directory\Background\shell\Powershell" -ot reg -actn setowner -ownr "n:Administrators"
+SetACL.exe -on "HKEY_CLASSES_ROOT\Directory\Background\shell\Powershell" -ot reg -actn ace -ace "n:Administrators;p:full"
+
+SetACL.exe -on "HKEY_CLASSES_ROOT\Directory\shell\Powershell" -ot reg -actn setowner -ownr "n:Administrators"
+SetACL.exe -on "HKEY_CLASSES_ROOT\Directory\shell\Powershell" -ot reg -actn ace -ace "n:Administrators;p:full"
+
+SetACL.exe -on "HKEY_CLASSES_ROOT\Directory\shell\Powershell\command" -ot reg -actn setowner -ownr "n:Administrators"
+SetACL.exe -on "HKEY_CLASSES_ROOT\Directory\shell\Powershell\command" -ot reg -actn ace -ace "n:Administrators;p:full"
+
+SetACL.exe -on "HKEY_CLASSES_ROOT\Directory\Background\shell\Powershell\command" -ot reg -actn setowner -ownr "n:Administrators"
+SetACL.exe -on "HKEY_CLASSES_ROOT\Directory\Background\shell\Powershell\command" -ot reg -actn ace -ace "n:Administrators;p:full"
+
+SetACL.exe -on "HKEY_CLASSES_ROOT\Drive\shell\Powershell" -ot reg -actn setowner -ownr "n:Administrators"
+SetACL.exe -on "HKEY_CLASSES_ROOT\Drive\shell\Powershell" -ot reg -actn ace -ace "n:Administrators;p:full"
+
+SetACL.exe -on "HKEY_CLASSES_ROOT\Drive\shell\Powershell\command" -ot reg -actn setowner -ownr "n:Administrators"
+SetACL.exe -on "HKEY_CLASSES_ROOT\Drive\shell\Powershell\command" -ot reg -actn ace -ace "n:Administrators;p:full"
+
+reg import add_powershell.reg
